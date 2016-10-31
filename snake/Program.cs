043ec72bfa3ec5258df05a5fe504171dcc15b98e
nikgal <nikgal00@gmail.com>
2016-10-31 22:28:51 +0300

    lines

--- snake/Program.cs
+++ snake/Program.cs
@@ -9,16 +9,21 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p = new Point(1,5, '#');
-           
+            Console.SetBufferSize(80, 25);
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            Point p = new Point(4, 5, '*');
             p.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-                
-            line.Draw();
-
-            VerticalLine vLine = new VerticalLine(5, 10, 8, '*');
-            vLine.Draw();
 
             Console.ReadLine();
         }
