79090c7adafa7ff83c4efa9bb253e26092e366ef
nikgal <nikgal00@gmail.com>
2016-10-31 20:42:03 +0300

    procedure

---
+++ snake/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+            char sym = '#';
+            Draw(x, y, sym);
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
