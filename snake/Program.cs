575056c16b5013a4cab1d5a33811c83eb8d5db42
nikgal <nikgal00@gmail.com>
2016-10-31 21:40:09 +0300

    list

--- snake/Program.cs
+++ snake/Program.cs
@@ -12,6 +12,17 @@ namespace snake
             Point p = new Point(1,5, '#');
            
             p.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.ReadLine();
         }
         
